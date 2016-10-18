54dc214d2ea4863d5b77f1ac1b8edd470e230c9a
adamerow <adamerow@vcd-ch204-08.eastern.ewu.edu>
2016-10-18 13:28:54 -0700

    fitvids plugin

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// My Scripts
+// Basic FitVids Test
+        $(".container").fitVids();
+        // Custom selector and No-Double-Wrapping Prevention Test
+        $(".container").fitVids({ customSelector: "iframe[src^='http://socialcam.com']"});
